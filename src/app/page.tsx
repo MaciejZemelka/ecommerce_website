7bdeb8b7f1b733020634269dee96478faee2b097
unknown <maciejzemelka@gmail.com>
2024-07-02 20:20:02 +0200

    Fixed bugs with animations on frontpage. Changed image for shoes. Redesgined left aside.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,43 +2,73 @@
 
 import Image from "next/image";
 import Header from "./components/header";
-import {useState} from "react";
+import { useState } from "react";
 import "./globals.css";
 
 export default function Home() {
   const [ShoeImage, SetShoeImage] = useState("/IMG/bluebut.png");
   const [animation, SetAnimation] = useState("");
 
-  const changeImg = (id:Number)=>{
+  const changeImg = (id: Number) => {
     let element = document.querySelector("#shoediv");
-   
-    
-    switch(id){
+    let bgelement = document.querySelector("#bg_color");
+    bgelement?.classList.forEach(className => {
+      if (className.startsWith('bg')) {
+        bgelement.classList.remove(className);
+      }
+    });
+    document.querySelector("#blue")?.classList.remove("border-[3px]");
+    document.querySelector("#red")?.classList.remove("border-[3px]");
+    document.querySelector("#purple")?.classList.remove("border-[3px]");
+    document.querySelector("#green")?.classList.remove("border-[3px]");
+    switch (id) {
       case 0:
-        
+        element?.classList.remove("shoeAnimation");
+        bgelement?.classList.add("bgblue");
         SetShoeImage("/IMG/bluebut.png");
-        element?.classList.add("shoeAnimation");
-        setTimeout(()=>element?.classList.remove("shoeAnimation"),2000)
+        setTimeout(() => element?.classList.add("shoeAnimation"), 1);
+        document.querySelector("#blue")?.classList.add("border-[3px]");
+        
         break;
       case 1:
         element?.classList.remove("shoeAnimation")
+        bgelement?.classList.add("bgpurple");
+        SetShoeImage("/IMG/purplebut.png");
+        setTimeout(() => element?.classList.add("shoeAnimation"), 1);
+        document.querySelector("#purple")?.classList.add("border-[3px]");
+        break;
+      case 2:
+        element?.classList.remove("shoeAnimation")
+        bgelement?.classList.add("bgred");
         SetShoeImage("/IMG/redbut.png");
-        element?.classList.add("shoeAnimation");
-        setTimeout(()=>element?.classList.remove("shoeAnimation"),2000)
+        setTimeout(() => element?.classList.add("shoeAnimation"), 1);
+        document.querySelector("#red")?.classList.add("border-[3px]");
+        break;
+      case 3:
+        element?.classList.remove("shoeAnimation")
+        bgelement?.classList.add("bggreen");
+        SetShoeImage("/IMG/greenbut.png");
+        setTimeout(() => element?.classList.add("shoeAnimation"), 1);
+        document.querySelector("#green")?.classList.add("border-[3px]");
+
         break;
     }
-    
+
   }
   return (
-    <main className="bg-[#00f] overflow-hidden ">
+    <main className="bgblue overflow-hidden " id="bg_color">
       <Header />
       <div className="h-[833px] flex justify-center pt-[150px] text-[24px]">
         <div className="">
-          <div className="absolute left-[100px] top-[360px] text-white font-bold">
-            <a>Jordan 1 | MID</a><br />
-            <a>Relase date: 2021</a><br />
-            <a>Price: 200$</a>
-            <div className="grid grid-cols-4 pt-[40px]  gap-4">
+          <div className="absolute left-[100px] top-[290px] text-white font-bold">
+            <div className="gap-y-[100px]">
+              <a className="text-[40px] text-bold ">Jordan 1 | MID</a><br /><br />
+              <a className=""><span className="text-[36px] text-bold">Relase date</span> <br />
+                <span className="text-[20px]">2021-09-18</span></a><br /><br />
+              <a><span className="text-[36px] text-bold">Price</span> <br />
+                <span className="text-[20px]">200$</span></a><br />
+            </div>
+            <div className="grid grid-cols-5   pt-[40px]  gap-4">
               <div className="p-3 border-[1px] border-white flex justify-center">
                 <a >8</a>
               </div>
@@ -70,24 +100,26 @@ export default function Home() {
                 <a >12.5</a>
               </div>
             </div>
-            <div className="flex pt-[40px] gap-x-5">
-              <div className="w-[70px] h-[70px] rounded-[360px] bg-[#00f] border-[3px] border-white cursor-pointer" onClick={()=>changeImg(0)}></div>
-              <div className="w-[70px] h-[70px] rounded-[360px] bg-[#f0f] border-[3px] border-white cursor-pointer" onClick={()=>changeImg(1)}></div>
-              <div className="w-[70px] h-[70px] rounded-[360px] bg-[#f00] border-[3px] border-white"></div>
-              <div className="w-[70px] h-[70px] rounded-[360px] bg-[#0f0] border-[3px] border-white"></div>
-              
+            <div className="pt-[10px]">
+              <a className="text-[36px] text-bold"> Select Color</a>
+            </div>
+            <div className="flex pt-[10px] gap-x-5">
+              <div className="w-[50px] h-[50px] rounded-[360px] bg-[#00f] border-[3px] cursor-pointer" id="blue" onClick={() => changeImg(0)}></div>
+              <div className="w-[50px] h-[50px] rounded-[360px] bg-[#f0f] cursor-pointer" id="purple" onClick={() => changeImg(1)}></div>
+              <div className="w-[50px] h-[50px] rounded-[360px] bg-[#f00] " id="red" onClick={() => changeImg(2)}></div>
+              <div className="w-[50px] h-[50px] rounded-[360px] bg-[#0f0] " id="green" onClick={() => changeImg(3  )}></div>
             </div>
           </div>
-          <div className="relative flex justify-center">
-            <div className="z-10 absolute  text-white text-[340px] font-bold tracking-wide">
-              <a className="">NIKE</a>
+          <div className="relative flex justify-center ">
+            <div className="z-10 absolute top-[-200px] text-white text-[500px] font-bold tracking-wide">
+              <a className="pt-narrow-bold tracking-tighter">NIKE</a>
             </div>
-            <div className="z-20 absolute left-[-190px] w-[600px] h-[7000px] " id="shoediv">
+            <div className="z-20 absolute left-[-240px] w-[600px] top-[-100px] " id="shoediv">
               <Image
                 src={ShoeImage}
                 alt="but"
-                width={400}
-                height={500}
+                width={500}
+                height={400}
                 id="but"
               />
             </div>
