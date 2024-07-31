ecbb93dbfd808708dcc7966afa264296dc1398a9
unknown <maciejzemelka@gmail.com>
2024-07-31 16:27:06 +0200

    Added email validation and CreatedDate field for fetching (register/page.tsx)

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 
 import { useState } from "react";
 import "../globals.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../store/slices/authSlice';
+import { AppDispatch, RootState } from '../store/store';
 
 export default function Home() {
     const [eyeicon, setEyeicon] = useState("/IMG/eyeicon.png");
@@ -14,6 +17,9 @@ export default function Home() {
     const [Password, setPassword] = useState("");
     const [ConPassword, setConPassword] = useState("");
     const [registerError, setRegisterError] = useState("");
+    const [EmailError, setEmailError] = useState(0);
+    const dispatch = useDispatch<AppDispatch>(); 
+     const { user, loading, error } = useSelector((state: RootState) => state.auth);
 
     const showPassword = () => {
         const element = document.querySelector("#password") as HTMLInputElement;
@@ -30,35 +36,67 @@ export default function Home() {
         }
     }
 
-    const register = async () => {
-        const response =  await fetch('https://localhost:7084/api/Registration/registration', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                id: 0,
-                Username: Username,
-                Email: Email,
-                Password: Password,
-                Permission: "user"
-            }),
-        });
+    const validateEmail = (Email: string) => {
+        return String(Email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+    
+    const currentTime = () =>{
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth()+1;
+        let day = date.getDate();
+        let hour = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
 
-        const data = await response.text();
-        if (data === "Error") alert("Error")
-        else if(data === "Data inserted") alert("git");
-        alert(data);
+        let time=year+"-"+month+"-"+day+" "+hour+":"+minutes+":"+seconds;
+        return time;
+    }
+    const register = async () => {
+           
+            setEmailError(0);
+            const response = await fetch('https://localhost:7084/api/Registration/registration', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    id: 0,
+                    Username: Username,
+                    Email: Email,
+                    Password: Password,
+                    Permission: "user",
+                    CreatedDate: currentTime()
+                }),
+            });
+            
+            const data = await response.text();
+            if (data === "Error") {
+                setRegisterError("Error")
+            }
+            else if (data === "Data inserted") {
+                dispatch(loginUser({ Email, Password }))
+                window.location.href = "/";
+            }
     }
 
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        if(Password!=ConPassword){
+        setEmailError(0);
+        if (!validateEmail(Email)) {
+            setEmailError(1);
+        }
+        else  if (Password != ConPassword) {
             setRegisterError("diffpass");
         }
-        else{
+        else {
+            setRegisterError("");
             register();
         }
-        
+
     }
 
     return (
@@ -67,9 +105,9 @@ export default function Home() {
                 <div className=" h-[650px] bg-white text-center pt-[40px] text-[24px]">
                     <form onSubmit={handleSubmit} className="space-y-[10px]">
                         <p className="font-bold text-[30px]">Sign Up</p>
-                        <input type="text" placeholder="username" className="w-[60%] px-[6px] py-[8px] border-[1px] border-black bglogin" onChange={(e) => setUsername(e.target.value)} /><br />
-                        <input type="text" placeholder="email" className="w-[60%] px-[6px] py-[8px] border-[1px] border-black bglogin"onChange={(e) => setEmail(e.target.value)} />
-                        <input type={inputType} placeholder="password" id="password" className="w-[60%] py-[8px] px-[6px] border-[1px] border-black bglogin" onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="text" placeholder="username" className="w-[60%] px-[6px] py-[8px] border-[1px] border-black bglogin" required onChange={(e) => setUsername(e.target.value)} /><br />
+                        <input type="text" placeholder="email" className="w-[60%] px-[6px] py-[8px] border-[1px] border-black bglogin" required onChange={(e) => setEmail(e.target.value)}  />
+                        <input type={inputType} placeholder="password" id="password" className="w-[60%] py-[8px] px-[6px] border-[1px] border-black bglogin" required onChange={(e) => setPassword(e.target.value)} />
                         <button className="absolute mx-2  border-[1px] border-black bglogin" onClick={() => showPassword()} type="button">
                             <Image
                                 src={eyeicon}
@@ -79,18 +117,13 @@ export default function Home() {
                                 id="showpass"
                             />
                         </button><br />
-                        <input type="password" placeholder="confirm password" id="confirm password" className="w-[60%] py-[8px] px-[6px] border-[1px] border-black bglogin" onChange={(e) => setConPassword(e.target.value)}/>
+                        <input type="password" placeholder="confirm password" id="confirm password" className="w-[60%] py-[8px] px-[6px] border-[1px] border-black bglogin" required onChange={(e) => setConPassword(e.target.value)} />
                         <div>
-                            {registerError === "diffpass" ? (<p className="text-[15px] text-[#f00] text-left w-[80%] px-[20%]">Password doesn't match</p>):(<p></p>)}
+                            {registerError === "diffpass" ? (<p className="text-[15px] text-[#f00] text-left w-[80%] px-[20%]">Password doesn't match</p>) : (<p></p>)}
+                            {EmailError === 1 ? (<p className="text-[15px] text-[#f00] text-left w-[80%] px-[20%]">It is not an email!</p>) : (<p></p>)}
+                            {registerError === "Error" ? (<p className="text-[15px] text-[#f00] text-left w-[80%] px-[20%]">Something went wrong, try again later.</p>) : (<p></p>)}
                         </div>
-                        <div className="text-left w-[80%] px-[20%]">
-                            <a href="password" className="text-[15px] text-[#00f]">Forgot password?</a>
-                        </div>
-                        <div className="flex  w-[100%] px-[20%] items-center" >
-                            <div className="w-[50%] flex justify-between pr-[30px] ">
-                                <input type="checkbox" className="w-[15px] h-[15px] mt-[10px] bglogin" />
-                                <p className="text-[20px]">remember me</p>
-                            </div>
+                        <div className="  w-[100%] px-[20%] items-center text-center" >
                             <button className="w-[50%] border-[1px] border-black px-[10px] bglogin" type="submit">
                                 Sign up
                             </button>
